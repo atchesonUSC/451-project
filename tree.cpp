75a91f1a3a01c9bc2a65ac31d408a8cbe677187c
Samuel Atcheson <atcheson@usc.edu>
2021-04-18 13:19:41 -0700

    merged Alex NNS function into RRTTree class

--- tree.cpp
+++ tree.cpp
@@ -1,6 +1,26 @@
 #include "tree.h"
 #include "math.h"
 
+
+// ========== Miscellaneous ========== //
+
+double distance(std::pair<double, double> pos1, std::pair<double, double> pos2) {
+    double x_diff = abs(pos1.first - pos2.first);
+    double y_diff = abs(pos1.second - pos2.second);
+    return sqrt(pow(x_diff, 2) + std::pow(y_diff, 2));    
+}
+
+// reduction phase function
+struct idx_val_pair distance_redux(const struct idx_val_pair omp_out, const struct idx_val_pair omp_in) {
+	if (omp_out.val < omp_in.val) {
+		return omp_out;
+	} else {
+		return omp_in;
+	}
+}
+
+// ========== RRTNode ========== //
+
 RRTNode::RRTNode(int idx, int p, std::vector<int> c, std::pair<double, double> pos) : id(idx), parent(p), children(c), position(pos) {}
 
 int RRTNode::getId(){
@@ -27,36 +47,11 @@ std::pair<double, double> RRTNode::getPosition(){
     return position;
 }
 
+// ========== RRTTree ========== //
+
 RRTTree::RRTTree() {}
 
-RRTTree::~RRTTree(){}
-
-double distance(std::pair<double, double> pos1, std::pair<double, double> pos2){
-    double x_diff = abs(pos1.first - pos2.first);
-    double y_diff = abs(pos1.second - pos2.second);
-    return sqrt(pow(x_diff, 2) + std::pow(y_diff, 2));    
-}
-
-// RRTNode RRTTree::nearestNeighbor(std::pair<double, double> pos, int start_idx, int end_idx){
-//     ReadLock r_lock(treeLock);
-
-//     if(start_idx < 0 || end_idx >= (int)nodes.size() || end_idx <= start_idx){
-//         //return (RRTNode*) nullptr; 
-//         //FIXME how to handle bad indices
-//     }
-
-//     int nearestIndex = start_idx;
-//     double nearestDistance = distance(pos, nodes[start_idx].getPosition());
-//     for(int i = start_idx; i < end_idx; i++){
-//         double dist = distance(pos, nodes[i].getPosition());
-//         if(dist < nearestDistance){
-//             nearestIndex = i;
-//             nearestDistance = dist;
-//         }
-//     }
-
-//     return nodes[nearestIndex];
-// }
+RRTTree::~RRTTree() {}
 
 void RRTTree::addNode(int p, std::pair<double, double> pos){
     //TODO synchronize this with a mutex
@@ -69,31 +64,14 @@ void RRTTree::addNode(int p, std::pair<double, double> pos){
     return;
 }
 
-/* === Nearest Neighbor Search === */
-
-// useful for the reduction phase
-struct idx_val_pair {
-	int idx;
-	double val;
-};
-
-// reduction phase function
-struct idx_val_pair find_min_distance(const struct idx_val_pair omp_out, const struct idx_val_pair omp_in) {
-	if (omp_out.val < omp_in.val) {
-		return omp_out;
-	} else {
-		return omp_in;
-	}
-}
-
-int nearest_neighbor_search(std::pair<double, double> pos, int t, int tree_sz) {
+RRTNode RRTTREE::nearest_neighbor_search(std::pair<double, double> p, int t) {
 	// set number of threads for openMP execution
 	omp_set_num_threads(t);
 
 	// declare reduction clause for finding index with nearest neighbor
 	#pragma omp declare reduction \
 		(min_pair : struct idx_val_pair : \
-		omp_out = find_min_distance(omp_out, omp_in)) \
+		omp_out = distance_redux(omp_out, omp_in)) \
 		initializer(omp_priv = {0, 0})
 
 	/*
@@ -108,9 +86,10 @@ int nearest_neighbor_search(std::pair<double, double> pos, int t, int tree_sz) {
 	struct idx_val_pair min = {0, INT_MAX};
 	
 	#pragma omp parallel for reduction(min_pair: min)
-	for (int i = 0; i < tree_sz; ++i) {
-		// calculate distance between random node and tree node
-		double d = distance(pos, nodes[i]);
+	for (int i = 0; i < nodes.size(); ++i) {
+		
+		// calculate distance between sampled node and tree node
+		double d = distance(p, nodes[i].getPosition());
 
 		// check if distance is less than the current minimum distance
 		if (d < min.val) {
@@ -118,4 +97,7 @@ int nearest_neighbor_search(std::pair<double, double> pos, int t, int tree_sz) {
 			min.val = d;
 		}
 	}
+
+	// return index of the nearest neigbor in the nodes vector
+	return nodes[min.idx];
 }
--- tree.h
+++ tree.h
@@ -5,38 +5,44 @@
 #include <limits.h>
 #include <algorithm>
 
-class RRTNode 
-{
+// ========== Miscellaneous items ========== //
+
+// useful for the reduction phase
+struct idx_val_pair {
+    int idx;
+    double val;
+};
+
+// reduction phase function
+struct idx_val_pair distance_redux(const struct idx_val_pair omp_out, const struct idx_val_pair omp_in);
+
+// ========== RRTNode class ========== //
+
+class RRTNode {
 public:
     RRTNode(int idx, int p, std::vector<int> c, std::pair<double, double> pos);
-
     int getId();
-
     int getParent();
     void setParent(int p);
-
     std::vector<int> getChildren();
     void addChild(int child_idx);
-
     std::pair<double, double> getPosition();
     
 private:
-    int id; //index of RRTNode in tree vector
-    int parent; //index of parent in tree vector;
-    std::vector<int> children; //indices of children in tree vector;
+    int id; 			          //index of RRTNode in tree vector
+    int parent; 		          //index of parent in tree vector;
+    std::vector<int> children;    //indices of children in tree vector;
     std::pair<double, double> position;
 };
 
-class RRTTree
-{
+// ========== RRTTree class ========== //
+
+class RRTTree {
 public:
     RRTTree(std::pair<double, double> start_pos); //FIXME create root somewhere
     ~RRTTree();
-
-    RRTNode nearestNeighbor(std::pair<double, double> pos, int start_idx, int end_idx);
-
+    RRTNode nearest_neighbor_search(std::pair<double, double> pos, int openmp_thread_count);
     void addNode(int p, std::pair<double, double> pos);
-
     int size();
 
 private:
